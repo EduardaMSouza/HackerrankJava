eb4bb8c8c1a40f7acfa80d1acb9874ffc1d2f95d
Eduarda <eduardasouza111213@gmail.com>
2024-05-14 19:15:11 -0300

    Java 2D Array challenge done.

---
+++ src/Java2DArray/Java2DArray.java
@@ -0,0 +1,48 @@
+package Java2DArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Java2DArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        System.out.println(findMaxHourGlasses(arr));
+
+        bufferedReader.close();
+    }
+
+    private static int findMaxHourGlasses(List<List<Integer>> arr) {
+        int maxHourGlasses = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int tempMaxHourGlasses = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + arr.get(i+1).get(j+1) + arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+                if (tempMaxHourGlasses > maxHourGlasses || (i == 0 && j ==0)) {
+                    maxHourGlasses = tempMaxHourGlasses;
+                }
+            }
+        }
+
+        return maxHourGlasses;
+    }
+}
+
